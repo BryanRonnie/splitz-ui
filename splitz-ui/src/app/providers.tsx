a82385deee205bad3bf155d19e1dbd4e728912e0
Bryan Ronnie <60704666+BryanRonnie@users.noreply.github.com>
2025-12-18 22:50:37 +0530

    trimming

--- splitz-ui/src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ReactNode } from 'react';
-import { SplitProvider } from '@/context/SplitContext';
-
-export default function Providers({ children }: { children: ReactNode }) {
-  return <SplitProvider>{children}</SplitProvider>;
-}
